3f3368d66eb0ca81690c7e31cae349c0b1f2b967
Arij <a.maaroufi@hannibalmakeapp.com>
2020-04-18 03:18:52 +0200

    make login working (save tokens)

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AuthService } from '../api/auth.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor(public toastController: ToastController, 
+    private _authService: AuthService,
+    private _storage: Storage,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    
+    console.log("start")
+    console.log(this.loginForm.value);
+    const email = this.loginForm.value.email
+    const password = this.loginForm.value.password
+    this._authService.login(email,password).subscribe(
+      async data => {
+          console.log(data)
+          this._storage.set('ACCESS_TOKEN',data.access_token)
+          this._storage.set('EXPIRES_AT',data.expires_at)
+          const tos = await this.toastController.create({
+            message: 'succefully connected....',
+            duration: 5000,
+            color: "success"
+          })
+          tos.present()
+          this.router.navigate(['/home'])
+      },
+      async error => {
+        console.log(error)
+        let errorMsg = 'error while trying to connect'
+        if(error.error.message){
+          errorMsg = error.error.message
+        }
+        if(error.status == 401){
+          errorMsg = 'Wrong email or passowrd'
+        }
+        
+        const tos = await this.toastController.create({
+          message: errorMsg,
+          duration: 5000,
+          color: "danger"
+        })
+        tos.present()
+      }
+    )
   }
 
 }
